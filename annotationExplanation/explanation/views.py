d8fa1c24d4d01fa4b5bcc6cb74085025782c0bf9
Stefano Passaro <stefano.passaro@gmail.com>
2021-09-28 14:14:10 +0200

    more explanations

--- annotationExplanation/explanation/views.py
+++ annotationExplanation/explanation/views.py
@@ -5,11 +5,15 @@ from django.db.models import Sum
 # Create your views here.
 
 def donatori(request):
-    donordata = Donor.objects.annotate(tot_donations = Sum('donations__amount')).order_by('tot_donations')
+    donordata = Donor.objects.annotate(tot_donations = Sum('donations__amount'))
     context = {'donor': donordata}
     return render(request, 'donors.html', context)
 
-    #Inutile attualmente
-    # donation_donor = Donation.objects.values('donor_id')
-    #Inutile attualmente
-    # donation_count = Donation.objects.values('donor_id').annotate(total=Sum('amount'))
+#count donations
+# Donor.objects.annotate(tot_donations = Sum('donations__amount'), num_donations = Count('donations'))
+
+# donations per year
+# Donor.objects.values('donations__cost_center').annotate(tot_per_year = Sum('donations__amount'))
+
+#donations per year and donor: 
+# Donation.objects.values('donor').values('cost_center', 'donor').annotate(tot_per_year = Sum('amount'))
