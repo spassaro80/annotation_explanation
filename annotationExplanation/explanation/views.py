599fc35812bb197a88f70338e88d56fa24384a9b
Stefano Passaro <stefano.passaro@gmail.com>
2021-10-06 07:37:46 +0200

    #donations per year with 0 sum

--- annotationExplanation/explanation/views.py
+++ annotationExplanation/explanation/views.py
@@ -6,7 +6,15 @@ from django.db.models import Sum
 
 def donatori(request):
     donordata = Donor.objects.annotate(tot_donations = Sum('donations__amount'))
-    context = {'donor': donordata}
+    donors_2019 = Donor.objects.filter(donations__cost_center = "2019").annotate(tot_2019 = Sum('donations__amount')).values()
+    donors_dict = donordata.values()
+    for donor in donors_dict:
+        if donors_2019.filter(id=donor['id']).exists():
+            donor['tot_2019'] = donors_2019.filter(id=donor['id']).values()[0]['tot_2019']
+        else:
+            donor['tot_2019'] = 0
+
+    context = {'donor': donorsdict}
     return render(request, 'donors.html', context)
 
 #count donations
@@ -21,3 +29,16 @@ def donatori(request):
 
 #donations per year and donor: 
 # Donation.objects.values('donor').values('cost_center', 'donor').annotate(tot_per_year = Sum('amount'))
+
+#donations per year with 0 sum
+
+# donors_2019 = Donor.objects.filter(donations__cost_center = "2019").annotate(tot_2019 = Sum('donations__amount')).values()
+# donors_dict = donordata.values()
+# for donor in donors_dict:
+#     if donors_2019.filter(id=donor['id']).exists():
+#         donor['tot_2019'] = donors_2019.filter(id=donor['id']).values()[0]['tot_2019']
+#     else:
+#         donor['tot_2019'] = 0
+
+# context = {'donor': donorsdict}
+# return render(request, 'donors.html', context)
